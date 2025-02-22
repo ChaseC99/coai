2b473a5777d40fe12cefb1d17ebc7edc14c5e6ed
Chase Carnaroli <chasecarnaroli@gmail.com>
2025-02-22 14:24:58 -0800

    Clean up state management

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
 import './App.css'
 import useCoai from './hooks/use-coai'
-import { Action } from './coai'
+import { CoaiState } from './coai'
 
 function App() {
-  const { image, triggerAction } = useCoai()
+
+  const { image, updateState } = useCoai()
 
   return (
     <>
       <div style={{display: 'flex', gap: '10px'}}>
-        <button onClick={() => triggerAction(Action.PET)}>PET</button>
-        <button onClick={() => triggerAction(Action.SCOLD)}>SCOLD</button>
+        <button onClick={() => updateState(CoaiState.HAPPY)}>PET</button>
+        <button onClick={() => updateState(CoaiState.MAD)}>SCOLD</button>
       </div>
       
       <div style={{ height: '50px' }}></div>
--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import { BrowserRouter, Route, Routes } from "react-router";
+import { BrowserRouter, Route, Routes, Navigate } from "react-router";
 import { BasicProvider } from '@basictech/react';
 import { schema } from '../basic.config';
 
@@ -12,7 +12,7 @@ createRoot(document.getElementById('root')!).render(
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<App />} />
-          <Route path="*" element={<div>404 - Page Not Found</div>} />
+          <Route path="*" element={<Navigate to="/" replace />} />
         </Routes>
       </BrowserRouter>
     </BasicProvider>
